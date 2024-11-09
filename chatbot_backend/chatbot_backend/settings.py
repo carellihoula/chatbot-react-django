0cc8f5026ea9ca5c791659e24ac52d6b7ac708a6
carellihoula <carellihoula10@gmail.com>
2024-11-09 22:00:27 +0100

    refactor : proctect the secret_key

--- chatbot_backend/chatbot_backend/settings.py
+++ chatbot_backend/chatbot_backend/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d*rk(#07eo6#l&zru1)s7hq7un5+k(^r-5eba@iws75f_e^esw'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
